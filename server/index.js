d94585f7a5fb2f472f4c5b0c2f9f538fc88f9ad8
Glacial <kli-van@yandex.ru>
2018-12-13 22:26:06 +0300

    pagination works

--- server/index.js
+++ server/index.js
@@ -20,36 +20,5 @@ app.listen(port, function () {
     console.log('Express server listening to port ' + port);
 });
 
-import chat_widget from "./src/modules/chat_widget"
-chat_widget(app, dialogflow)
-
-// app.get('/chatbot', (req, res) => {
-//   let text = req.query.text
-//   talk(text).then((answer) => {
-//     res.send(
-//       {
-//         answer: answer
-//       }
-//     )
-//   })
-// })
-
-// function talk(text) {
-//   return new Promise((resolve, reject) => {
-//     let talkRequest = dialogflow.textRequest(text, {
-//       sessionId: 'Canadian_bot_talk_to_you'
-//     });
-
-//     talkRequest.on('response', function (response) {
-//       let answer = response.result.fulfillment.speech
-//       if(answer === '') answer = "Я не знаю, что тебе на это ответить."
-//       resolve(answer)
-//     });
-
-//     talkRequest.on('error', function (error) {
-//       reject(error)
-//     });
-
-//     talkRequest.end();
-//   })
-// }
+import chat_widget from "./src/modules/chat_widget";
+chat_widget(app, dialogflow);
