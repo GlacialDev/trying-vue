51fa4a21284e674055e1f88417f409eed9954e0f
Glacial <kli-van@yandex.ru>
2018-12-16 22:03:23 +0300

    blog works as it should

--- server/src/modules/database/api_blog.js
+++ server/src/modules/database/api_blog.js
@@ -2,31 +2,36 @@ import db from "./db"
 
 export default function api_blog(app) {
 
+    app.get('/blogposts_count', (req, res) => {
+        db.get().collection('blogposts').count().then((result) => {
+            res.send(""+result)
+        })
+    })
+
     app.get('/blogposts', (req, res) => {
-        db.get().collection('blogposts').find({}).toArray((err, result) => {
+        db.get().collection('blogposts').find({ id : { $gte : +req.query.firstPostIndex, $lte : +req.query.lastPostIndex } }).toArray((err, result) => {
             if (err) {
                 console.log(err)
                 return res.sendStatus(500)
             }
+            console.log(req.query.firstPostIndex+' '+req.query.lastPostIndex)
+            console.log(result)
             res.send(result)
         })
     })
 
-
     app.post('/blogposts', (req, res) => {
         let postContent = {
-            id: req.body.id,
-            date: req.body.date,
-            title: req.body.title,
-            text: req.body.text
+            id: +req.query.id,
+            date: req.query.date,
+            title: req.query.title,
+            text: req.query.text
         };
         db.get().collection('blogposts').insertOne(postContent, (err, result) => {
             if (err) {
                 console.log(err)
                 return res.sendStatus(500)
             }
-            console.log(result)
-            console.log("added")
             res.sendStatus(200)
         })
     })
